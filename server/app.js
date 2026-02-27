e935a7c9674256c9417617e817eed8dc30f8877a
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-26 19:00:47 -0500

    Add user and not-found routes and error handler to app.js

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 require('express-async-errors');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+app.use(cookieParser());
 
+const auth = require('./middleware/auth');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const notFound = require('./middleware/notFound');
 const errorHandler = require('./middleware/errorHandler');
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', auth, userRoutes);
 
+app.use(notFound);
 app.use(errorHandler);
 
 module.exports = app;
